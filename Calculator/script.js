be729307389d1fd3ea9b624a74854f4727621c13
Prachiti102005 <prachitikothekar@gmail.com>
2024-07-21 19:13:35 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,20 @@
+// Function to add content to the display
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  // Function to clear the display
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  // Function to evaluate the expression in the display
+  function calculate() {
+    let display = document.getElementById('display');
+    try {
+      display.value = eval(display.value);
+    } catch (error) {
+      display.value = 'Error';
+    }
+  }
+  
